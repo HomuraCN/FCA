e71124a6d62b2cabade0fa4390eee24ecd3163cd
芒果味 <ymm_2022@163.com>
2025-09-12 18:04:19 +0800

    feature/IRD_NRR

---
+++ src/test/java/fca/AnalysisTest.java
@@ -0,0 +1,84 @@
+package fca;
+
+import fca.analysis.Analysis;
+import fca.utils.Context;
+import fca.utils.concept.Concept;
+import fca.utils.readFile.BitSetFileHandler;
+import fca.utils.readFile.File;
+import fca.utils.readFile.LatticeFileHandler;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.BitSet;
+
+public class AnalysisTest {
+    @Test
+    void IRDTest() {
+        try {
+            ArrayList<BitSet> reductions_permutations = BitSetFileHandler.readFromFile("src/main/java/data/reduction_permutations/test_deduplication_reduction_permutations.data.txt");
+            ArrayList<Concept> originalLattice = LatticeFileHandler.readLatticeFromFile("src/main/java/data/lattice/test_deduplication_lattice.data.txt");
+            BitSet reductionPermutation = reductions_permutations.getFirst();
+
+            Context originalContext = File.readFile("src/main/java/data/context/test_deduplication.data.txt");
+            Context reverseContext = File.readFile("src/main/java/data/reverse_context/test_reverse.data.txt");
+
+            double ird = Analysis.calculateIRD(
+                    originalContext,
+                    reverseContext,
+                    reductionPermutation, // 注意这里传入的是单个BitSet
+                    originalLattice
+            );
+
+            System.out.println("信息还原度 (IRD): " + ird);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    @Test
+    void NRRTest() {
+        try {
+            ArrayList<Concept> originalLattice = LatticeFileHandler.readLatticeFromFile("src/main/java/data/lattice/test_deduplication_lattice.data.txt");
+            ArrayList<Concept> reverseLattice = LatticeFileHandler.readLatticeFromFile("src/main/java/data/reverse_lattice/test_lattice_reverse_extents.data.txt");
+
+            double nrr = Analysis.calculateNodeReductionRate(
+                    originalLattice,
+                    reverseLattice
+            );
+
+            System.out.println("节点减少率 (NRR): " + nrr);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    @Test
+    void AllTest(){
+        try {
+            ArrayList<BitSet> reductions_permutations = BitSetFileHandler.readFromFile("src/main/java/data/reduction_permutations/iris_deduplication_reduction_permutations.data.txt");
+            ArrayList<Concept> originalLattice = LatticeFileHandler.readLatticeFromFile("src/main/java/data/lattice/iris_deduplication_lattice.data.txt");
+            BitSet reductionPermutation = reductions_permutations.getFirst();
+
+            Context originalContext = File.readFile("src/main/java/data/context/iris_deduplication.data.txt");
+            Context reverseContext = File.readFile("src/main/java/data/reverse_context/iris_reverse.data.txt");
+
+            ArrayList<Concept> reverseLattice = LatticeFileHandler.readLatticeFromFile("src/main/java/data/reverse_lattice/iris_lattice_reverse_extents.data.txt");
+
+            double ird = Analysis.calculateIRD(
+                    originalContext,
+                    reverseContext,
+                    reductionPermutation, // 注意这里传入的是单个BitSet
+                    originalLattice
+            );
+
+            double nrr = Analysis.calculateNodeReductionRate(
+                    originalLattice,
+                    reverseLattice
+            );
+
+            System.out.println("信息还原度 (IRD): " + ird);
+            System.out.println("节点减少率 (NRR): " + nrr);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
