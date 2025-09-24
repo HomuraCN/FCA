7524fb410df4330a518da5e22f631c438f723f89
HomuraCN <18265395982@163.com>
2025-09-24 16:44:27 +0800

    add GraphBuilder A_negative

--- src/test/java/fca/GraphBuilderTest.java
+++ src/test/java/fca/GraphBuilderTest.java
@@ -2,7 +2,9 @@ package fca;
 
 import fca.algorithm.graph.GraphBuilder;
 import fca.utils.Context;
+import fca.utils.concept.AEConcept_id;
 import fca.utils.concept.Concept;
+import fca.utils.readFile.AELatticeFileHandler;
 import fca.utils.readFile.File;
 import fca.utils.readFile.LatticeFileHandler;
 import org.junit.jupiter.api.Test;
@@ -44,7 +46,7 @@ public class GraphBuilderTest {
     @Test
     public void testBuildAndWriteAdjacencyMatrix() {
         try {
-            String datasetName = "iris"; // 方便切换数据集
+            String datasetName = "test_deduplication"; // 方便切换数据集
 
             // 1. 加载形式背景
             Context context = File.readFile("src/main/java/data/context/" + datasetName + ".data.txt");
@@ -61,6 +63,31 @@ public class GraphBuilderTest {
             // 4. 将矩阵写入文件
             String outputFilePath = "src/main/java/data/graph/" + datasetName + "_A_plus.csv";
             GraphBuilder.writeMatrixToFile(a_plus, outputFilePath);
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    @Test
+    public void testBuildAndWriteAEAdjacencyMatrix(){
+        try {
+            String datasetName = "test_deduplication"; // 方便切换数据集
+
+            // 1. 加载形式背景
+            Context context = File.readFile("src/main/java/data/context/" + datasetName + ".data.txt");
+            int objectCount = context.getObjs_size();
+
+            // 2. 加载概念格
+            ArrayList<AEConcept_id> concepts = AELatticeFileHandler.readAELatticeFromFile("src/main/java/data/lattice3C/" + datasetName + "_aelattice.data.txt");
+
+            // 3. 构建邻接矩阵
+            System.out.println("正在构建邻接矩阵 A_negative...");
+            int[][] a_negative = GraphBuilder.buildAEAdjacencyMatrix(concepts, objectCount);
+            System.out.println("构建完成。");
+
+            // 4. 将矩阵写入文件
+            String outputFilePath = "src/main/java/data/graph/" + datasetName + "_A_negative.csv";
+            GraphBuilder.writeMatrixToFile(a_negative, outputFilePath);
 
         } catch (IOException e) {
             throw new RuntimeException(e);
